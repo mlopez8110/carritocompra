caaf5d1ae5b61cd511e1cea62033e83f9e080336
mlopez8110 <65571057+mlopez8110@users.noreply.github.com>
2023-08-21 10:33:51 -0500

    Update VentaController.java

--- backend/carrito-compra/src/main/java/com/carrito/compra/controller/VentaController.java
+++ backend/carrito-compra/src/main/java/com/carrito/compra/controller/VentaController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,6 +15,7 @@ import com.carrito.compra.model.Producto;
 import com.carrito.compra.model.Venta;
 import com.carrito.compra.service.VentaService;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api/v1/")
 public class VentaController {
